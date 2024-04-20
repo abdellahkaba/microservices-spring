0d6271879a473c9589880c018131fb2b769b212f
Abdellah <abdallahkaba98@gmail.com>
2024-04-20 23:15:23 +0000

    refresh-version

--- src/main/java/net/kaba/customerservice/config/GlobalConfig.java
+++ src/main/java/net/kaba/customerservice/config/GlobalConfig.java
@@ -1,11 +1,15 @@
 package net.kaba.customerservice.config;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 //creer un bean global config avec cette annotation
 //au demarrage  il creer un objet config avec des parametres de global param
 @ConfigurationProperties(prefix = "global.params")
-public record GlobalConfig(int p1,int p2) {
-
-
+@Data @NoArgsConstructor @AllArgsConstructor
+public class GlobalConfig {
+    private int p1 ;
+    private int p2 ;
 }
--- src/main/java/net/kaba/customerservice/web/ConfigTestController.java
+++ src/main/java/net/kaba/customerservice/web/ConfigTestController.java
@@ -3,12 +3,14 @@ package net.kaba.customerservice.web;
 import net.kaba.customerservice.config.GlobalConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
 @RestController
+
 public class ConfigTestController {
 
     //afficher les parametres et injecte depuis le fichier properties
